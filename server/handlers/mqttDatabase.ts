f472a9235a157aa310f5bd7ee5f5d9ef34a83294
Dan Miller <dsmiller95@gmail.com>
2016-11-16 17:03:04 -0600

    removed extra brace

--- server/handlers/mqttDatabase.ts
+++ server/handlers/mqttDatabase.ts
@@ -22,43 +22,42 @@ class mqttInit{
 		  client.subscribe('/dantest/info/temp')
 		});
 
-			client.on('message', (topic, message) => {  
-				if(topic === '/dantest/info/temp') {
+		client.on('message', (topic, message) => {  
+			if(topic === '/dantest/info/temp') {
 
-				console.log(message.toString());
-				try{
-					var obj = JSON.parse(message.toString());
-				}catch(exception){
-					console.log("Error parsing mqtt message:");
-					console.log(topic, message.toString());
-					console.log(exception);
-					return;
+			console.log(message.toString());
+			try{
+				var obj = JSON.parse(message.toString());
+			}catch(exception){
+				console.log("Error parsing mqtt message:");
+				console.log(topic, message.toString());
+				console.log(exception);
+				return;
+			}
+			
+
+			if(!(obj.temp && obj.time))
+				return;
+			}
+
+
+			var connection = getConnection();
+
+			connection.connect();
+			connection.query('INSERT INTO birdRecords.temp_info ' + 
+				'(temp, measurement_time) values' +
+				'(' + obj.temp + ', ' + obj.time + ')', function (err, rows, fields) {
+				if (err){
+					console.log("mysql error from mqtt");
+					console.log({error: err});
+				}else{
+					client.publish('/dantest/info/reporting', JSON.stringify({success: rows}));
 				}
-				
-
-				if(!(obj.temp && obj.time))
-					return;
-				}
-
-
-				var connection = getConnection();
-
-				connection.connect();
-				connection.query('INSERT INTO birdRecords.temp_info ' + 
-					'(temp, measurement_time) values' +
-					'(' + obj.temp + ', ' + obj.time + ')', function (err, rows, fields) {
-					if (err){
-						console.log("mysql error from mqtt");
-						console.log({error: err});
-					}else{
-						client.publish('/dantest/info/reporting', JSON.stringify({success: rows}));
-					}
-				});
-				connection.end();
-
-
 			});
-		}
+			connection.end();
+
+
+		});
 	}
 }
 
