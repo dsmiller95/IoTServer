de444cd7e2f38156e8f00ca5d994131ff4e7b30c
Dan Miller <dsmiller95@gmail.com>
2016-11-16 01:13:46 -0600

    fixed up sql script

--- server/sqlScripts/createUser.sql
+++ server/sqlScripts/createUser.sql
@@ -1,5 +1,4 @@
-DROP USER IF EXISTS 'default'@'localhost';
-CREATE USER 'default'@'localhost' IDENTIFIED BY 'secret';
+CREATE USER IF NOT EXISTS 'default'@'localhost' IDENTIFIED BY 'secret';
 GRANT INSERT ON birdrecords.* TO 'default'@'localhost';
 GRANT DELETE ON birdrecords.* TO 'default'@'localhost';
 GRANT SELECT ON birdrecords.* TO 'default'@'localhost';
